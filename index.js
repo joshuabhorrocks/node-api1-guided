f4d3e7864ccd9f693da551b32b5811795a16b95e
Joshua Horrocks <joshuabhorrocks@gmail.com>
2020-05-04 18:41:54 -0600

    CRUD operations work

--- index.js
+++ index.js
@@ -3,8 +3,39 @@ const express = require("express"); //CommonJS Modules
 
 const server = express();
 
+server.use(express.json()); // teaches express how to read JSON from the body
+
+let lessons = [
+    {
+        id: 1,
+        name: "Introduction to HTTP APIs with Node and Express"
+    }
+];
+
 server.get("/", (req, res) => {
     res.json({api: "Up and running!"})
+})
+
+server.get("/api/lessons", function(req, res){
+    // return an array of lessons (id, name)
+    res.json(lessons);
+});
+
+server.post("/api/lessons", function(req, res) {
+    const lessonInformation = req.body;
+
+    lessons.push(lessonInformation);
+
+    res.status(201).json(lessonInformation);
+})
+
+server.delete("/api/lessons/:id", function(req, res) {
+    const id = Number(req.params.id);
+
+    // find the lesson on the array and remove it
+    lessons = lessons.filter(lesson => lesson.id !== id)
+
+    res.status(200).json(lessons)
 })
 
 server.listen(8000, () => console.log("\nAPI is up\n"))
